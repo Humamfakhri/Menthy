5ec2f4dc8e944ea2819b2b908b02f534353878ec
Humamfakhri <humamifakhri@gmail.com>
2021-07-12 08:58:22 +0700

    Fix Tampilan Mobile

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,5 +1,7 @@
 const navbar = document.querySelector('.navbar');
 const headingHero = document.querySelector('.hero h1');
+const navToggler = document.querySelector(".navbar-toggler");
+
 document.addEventListener('scroll', ()=> {
     if(this.pageYOffset > headingHero.offsetTop - navbar.clientHeight){
         navbar.classList.add('scrolled')
@@ -7,3 +9,7 @@ document.addEventListener('scroll', ()=> {
         navbar.classList.remove('scrolled')
     }
 });
+
+navToggler.addEventListener('click', ()=> {
+    navToggler.classList.toggle("active")
+})
